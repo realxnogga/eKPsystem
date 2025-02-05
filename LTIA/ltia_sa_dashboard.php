330f8949b1c39becbaf9e877f599fa275d1c3508
Cortes J <jacobcortes46@gmail.com>
2025-02-05 14:17:43 +0800

    DGJDJG

--- LTIA/ltia_sa_dashboard.php
+++ LTIA/ltia_sa_dashboard.php
@@ -21,7 +21,7 @@ $municipalities = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LTIA Municipalities</title>
-    <link rel="icon" type="image/x-icon" href="../img/favicon.ico"> 
+    <link rel="icon" type="image/x-icon" href="../img/favicon.ico">
     <link rel="stylesheet" href="../assets/css/styles.min.css" />
 </head>
 
